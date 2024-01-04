790489c1868fb73fd6078720f03993cb58ff18d1
Raymond <raymond@zoee.com>
2024-01-04 01:42:56 -0500

    pizazz

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,10 @@ export function IndexPage() {
   return (
     <div className="container">
       <h1 className="font-bold text-6xl text-center text-indigo-600">
-        Welcome to <strong className="font-black">Zoee</strong>.
+        Welcome to{" "}
+        <strong className="font-black underline decoration-wavy decoration-2 underline-offset-8">
+          Zoee
+        </strong>.
       </h1>
     </div>
   );
